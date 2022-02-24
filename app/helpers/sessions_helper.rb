638e9888bd770b75301724c80ee8179225c3a6aa
raccoon <toshiki21320@icloud.com>
2022-02-24 05:01:02 +0000

    Finish editing-users

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,11 +4,6 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
-  
-  def log_out
-    session.delete(:user_id)
-    @current_user = nil
-  end
 
   
   def remember(user)
@@ -45,11 +40,29 @@ module SessionsHelper
     end
 
   end
+  
+  # 渡されたユーザーがログイン済みのユーザーであればtrueを返します。
+  def current_user?(user)
+    user == current_user
+  end
+
 
   # 現在ログイン中のユーザーがいればtrue、そうでなければfalseを返します。
   def logged_in?
     !current_user.nil?
   end
+  
+  # 記憶しているURL(またはデフォルトURL)にリダイレクトします。
+  def redirect_back_or(default_url)
+    redirect_to(session[:forwarding_url] || default_url)
+    session.delete(:forwarding_url)
+  end
+
+  # アクセスしようとしたURLを記憶します。
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+
 
   
   
