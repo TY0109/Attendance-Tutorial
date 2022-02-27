8dc60a2e38c02db06c576239a0706bae2f7b3258
raccoon <toshiki21320@icloud.com>
2022-02-27 12:23:54 +0000

    Finish japanization-and-time-format

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
 
+  attr_accessor :remember_token
   before_save { self.email = email.downcase }
+  
   validates :name,  presence: true, length: { maximum: 50 }
+  
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 100 },
                     format: { with: VALID_EMAIL_REGEX },
@@ -9,18 +12,6 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
-  
-  
-  # 渡された文字列のハッシュ値を返します。
-  def User.digest(string)
-    cost = 
-      if ActiveModel::SecurePassword.min_cost
-        BCrypt::Engine::MIN_COST
-      else
-        BCrypt::Engine.cost
-      end
-    BCrypt::Password.create(string, cost: cost)
-  end
 
   # 渡された文字列のハッシュ値を返します。
   def User.digest(string)
@@ -38,7 +29,7 @@ class User < ApplicationRecord
     SecureRandom.urlsafe_base64
   end
   
-  attr_accessor :remember_token
+  # 永続セッションのためハッシュ化したトークンをデータベースに記憶します。
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
